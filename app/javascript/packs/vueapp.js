600bfbf7fb4f64873946977a85e3fa0286479265
Jose Fernando Davila Orta <josefernandodavilaorta@MacBook-Pro-de-JoseFer.local>
2019-05-23 21:09:44 -0500

    vue configuration

---
+++ app/javascript/packs/vueapp.js
@@ -0,0 +1,14 @@
+import Vue from 'vue/dist/vue.esm'
+
+
+
+document.addEventListener('DOMContentLoaded', () => {
+  new Vue({
+    el: '#app',
+    data: {
+      message: 'Hello Vue!'
+    },
+    methods: {}
+  });
+});
+
