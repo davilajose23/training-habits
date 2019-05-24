5018998ca64779a106321e59af0e3e9a655b22e7
Jose Fernando Davila Orta <josefernandodavilaorta@MacBook-Pro-de-JoseFer.local>
2019-05-24 15:02:16 -0500

    create model activity_type

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
+  resources :activity_types
   devise_for :users
   get 'main/index'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_05_24_044728) do
+ActiveRecord::Schema.define(version: 2019_05_24_192256) do
+
+  create_table "activity_types", force: :cascade do |t|
+    t.string "name"
+    t.string "unit"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "users", force: :cascade do |t|
     t.string "email", default: "", null: false
