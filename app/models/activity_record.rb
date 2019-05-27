e02d02cc11062758639922295e504e54f13ded08
Jose Fernando Davila Orta <josefernandodavilaorta@MacBook-Pro-de-JoseFer.local>
2019-05-27 18:25:17 -0500

    add validation of not null attributes

--- app/models/activity_record.rb
+++ app/models/activity_record.rb
@@ -1,4 +1,5 @@
 class ActivityRecord < ApplicationRecord
   belongs_to :activity_type
   belongs_to :user
+  validates :description, :quantity, :activity_type_id, presence: true
 end
--- app/models/activity_type.rb
+++ app/models/activity_type.rb
@@ -1,6 +1,6 @@
 class ActivityType < ApplicationRecord
   has_many :activity_records, dependent: :destroy
-
+  validates :name, :unit, presence: true
   def name_and_quantity
     "#{self.name} (#{self.unit})"
   end
